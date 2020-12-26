cda04618083ea6c7ee9c0c4ec757c97509640349
Samuel Daza <samueldazadaza@gmail.com>
2020-12-26 02:07:53 -0500

    se crea app inicial

--- 01.js
+++ 01.js
@@ -1,32 +1,21 @@
-const app = new Vue({
+new Vue({
     el: '#app', //dentro de este id se trabajara con vue
-    data: { //abrir otro objeto
-        titulo: 'Aplicativo para crear inventario en empresa',
-        equipos: [ //contruyendo objetos dentro de array dentro de llaves
-            {nombre:'Laptops', cantidad:10},
-            {nombre:'Mesas', cantidad:0},
-            {nombre:'Sillas', cantidad:11},
-        ],
-        nuevoequipo: '',
-        total: 0
+    data: {
+        valorA: 0,
+        valorB: 0
     },
-    //creando metodos
     methods:{
-        agregarequipo () {
-            this.equipos.push({ //this = para accedes al data
-                nombre: this.nuevoequipo, cantidad:0 //trae datos desde input desde html
-            });
-            this.nuevoequipo = ''; // para borrar texto pegote de html del input
+        suma:function(){
+
+            return (this.valorA+this.valorB);
+        },
+        descuento:function(){
+
+            return (this.valorA * 0.04 );
+        },
+        descuento2:function(){
+
+            return ((this.valorA * 0.04) * 2 );
         }
-        
     },
-    computed:{
-        sumarequipos(){
-            this.total = 0;
-            for(item of this.equipos){
-                this.total = this.total + item.cantidad;
-            }
-            return this.total;
-        }
-    }
 })
