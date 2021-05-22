0ac9741c1c33e76af8e5ba149de234f42600decf
Samuel Daza <samueldazadaza@gmail.com>
2021-05-22 14:12:35 -0500

    canmiando interfaz y valore vue

--- 01.js
+++ 01.js
@@ -2,20 +2,25 @@ new Vue({
     el: '#app', //dentro de este id se trabajara con vue
     data: {
         valorA: 908526,
-        valorB: 0
+        valorB: 0,
+        transporte: 106000
     },
     methods:{
         suma:function(){
 
-            return (this.valorA+this.valorB);
+            return (this.valorA + this.valorB);
         },
         descuento:function(){
 
-            return ((this.suma * 0.08 / 2 ) );
+            return ((this.valorA + this.valorB) * 0.04);
         },
-        descuento2:function(){
+        descuentototal:function(){
 
-            return ((this.valorA * 0.04) * 2 );
+            return ((this.valorA + this.valorB) * 0.08);
+        },
+        netoapagar:function(){
+            return (((this.valorA + this.valorB) - ((this.valorA + this.valorB) * 0.08)));
         }
+       
     },
 })
